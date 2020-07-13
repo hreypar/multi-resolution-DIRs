9fb7155b7cb5df65eac94a92a254e280ee6e5035
hreypar <hreypar@gmail.com>
2020-07-13 14:58:00 -0500

    produce two heatmaps

--- bin/plot-DIRs-multiresolution-heatmap.R
+++ bin/plot-DIRs-multiresolution-heatmap.R
@@ -36,58 +36,69 @@ if (is.null(opt$input)){
 # it's dangerous to go alone! take this.
 #
 #################### resolution of the data ############################
-format_hicres <- function(hicres) { 
-  if(hicres >= 1000000) {
-    hicunit = paste0(hicres/1000000, "Mb")
-  } else {
-    hicunit = paste0(hicres/1000, "kb")
-  }
-}  
+# format_hicres <- function(hicres) { 
+#   if(hicres >= 1000000) {
+#     hicunit = paste0(hicres/1000000, "Mb")
+#   } else {
+#     hicunit = paste0(hicres/1000, "kb")
+#   }
+# }  
 #
 ########################################################################
 ######################### read in data #################################
 multiresolution.DIRs.summary <- do.call(rbind, lapply(opt$input, readRDS))
 #
-########################## change levels ###############################
+######################### format dataframe #############################
 #
 # AGAIN, THIS MODULE IS HIGHLY SPECIFIC TO THESE MCF10 CANCER PROGRESSION
 # MODEL DATA. NOTHING SHOULD BE HARDCODED LIKE THIS
+#
+# set DIRs levels so they show up properly in the plot
 multiresolution.DIRs.summary$DIRs <- factor(multiresolution.DIRs.summary$DIRs, 
-       levels = c("MCF-10AT1 vs MCF-10A -logFC", "MCF-10AT1 vs MCF-10A +logFC",
-                  "MCF-10CA1A vs MCF-10AT1 -logFC", "MCF-10CA1A vs MCF-10AT1 +logFC",
-                  "MCF-10CA1A vs MCF-10A -logFC", "MCF-10CA1A vs MCF-10A +logFC"))
+       levels = c("MCF-10AT1 vs MCF-10A -logFC", "MCF-10CA1A vs MCF-10AT1 -logFC",
+                  "MCF-10CA1A vs MCF-10A -logFC", "MCF-10AT1 vs MCF-10A +logFC",
+                  "MCF-10CA1A vs MCF-10AT1 +logFC", "MCF-10CA1A vs MCF-10A +logFC"))
+
+# remove the chr from chromosome so it doesn't clutter in the plot
+multiresolution.DIRs.summary$Chromosome <- gsub("chr", "", multiresolution.DIRs.summary$Chromosome)
+multiresolution.DIRs.summary$Chromosome <- factor(multiresolution.DIRs.summary$Chromosome, 
+                                                  levels = c(seq(1,22), "X"))
 #
 ######################### plot heatmap #################################
 ####### prepare variables for plot
 
 ####### plot1
-  # generate file
-  png(filename = opt$output, height = 9, width = 15, units = "in", res = 300)
-  
-  plot1 <- 
-    
-    ggplot(distance.fc, aes(D, logFC)) + 
-    geom_point(shape=20, alpha=0.6) + theme_minimal() +
-    geom_rug(col="steelblue",alpha=0.1, size=1.25) +
-    ggtitle(t.main) + xlab("Distance (bins)") + ylab("log fold-change")
-  
-  print(plot1)
-  dev.off()
-  
-####### plot2
-  # generate file
-  png(filename = paste0(outdir, "/", sigpairs, "-distance-vs-logfc-", r, ".png"),
-      height = 9, width = 15, units = "in", res = 300)
-  
-  plot2 <- ggplot(distance.fc, aes(D, logFC)) + 
-    geom_point(shape=20, alpha=0.6) +  facet_wrap(~chr) + #theme_minimal() +
-    geom_rug(col="steelblue",alpha=0.1, size=1.25) +
-    ggtitle(t.main) + xlab("Distance (bins)") + ylab("log fold-change")
-  
-  print(plot2)
-  dev.off()
+png(filename = opt$output, height = 9, width = 15, units = "in", res = 300)
 
+plot1 <- ggplot(multiresolution.DIRs.summary, 
+       aes(x = Chromosome, y = as.factor(Resolution), fill = Count)) +
+  geom_tile() + facet_wrap(~DIRs) + theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_fill_gradient(name = "DIR Count",
+                      low = "#BDC2BF",
+                      high = "#023364") +
+  ggtitle("Differentially Interacting Regions by Resolution\n") +
+  ylab("Resolution (bp)")
 
-  
+print(plot1)
+dev.off()
+
+####### plot1 log10
+png(filename = gsub(".png", "-log10.png", opt$output), 
+    height = 9, width = 15, units = "in", res = 300)
+
+plot1.log10 <- ggplot(multiresolution.DIRs.summary, 
+                aes(x = Chromosome, y = as.factor(Resolution), fill = log10(Count))) +
+  geom_tile() + facet_wrap(~DIRs) + theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_fill_gradient(name = "DIR Count (log10)",
+                      low = "#BDC2BF",
+                      high = "#023364") +
+  ggtitle("Differentially Interacting Regions by Resolution\n") +
+  ylab("Resolution (bp)")
+
+print(plot1.log10)
+
+dev.off()
   
   
